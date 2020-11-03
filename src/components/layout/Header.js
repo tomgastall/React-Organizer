9d40ce20838c8566ebd7c92639051401ce7ac777
tomgastall <tomgastall@gmail.com>
2020-11-03 11:53:57 -0800

    second commit

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -9,7 +9,7 @@ export const Header = () => {
     <header className="header" data-testid="header">
         <nav>
             <div className="logo">
-                <img src="/images/logo.png" alt="Todoist" />
+                <img src="/images/logo.png" alt="React Organizer" />
             </div>    
             <div className="settings">
                 <ul>
