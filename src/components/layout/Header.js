2e92dc4b967144e74502efb6c1932d80b1c90f08
tomgastall <tomgastall@gmail.com>
2020-11-02 18:38:53 -0800

    first commit

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -13,8 +13,11 @@ export const Header = () => {
             </div>    
             <div className="settings">
                 <ul>
-                    <li>+</li>
-                    <li><FaPizzaSlice /></li>
+                    <li data-testid="quick-add-task-action" className="settings__add">
+                        +
+                    </li>
+                    <li data-testid="dark-mode-action" className="settings__darkmode">
+                    <FaPizzaSlice /></li>
                 </ul>
             </div>
         </nav>
